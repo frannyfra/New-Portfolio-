6f5534843c3297c80307534bdbd82f70b44a0a66
frannyfra <francescadelaurentis1987@gmail.com>
2021-02-05 20:00:49 +0000

    Adjust navbar

--- myscript.js
+++ myscript.js
@@ -1,10 +1,10 @@
 
 const toggle = () => {
   let element = document.getElementById("myTopnav");
-  element.className === "topnav" ? 
-  element.className += " responsive" 
-  : 
-  element.className = "topnav";
+  element.className === "topnav" ?
+    element.className += " responsive"
+    :
+    element.className = "topnav";
 }
 
 let prevScrollpos = window.pageYOffset;
@@ -14,11 +14,13 @@ const navbar = document.getElementById("myTopnav");
   if (navbar.className === "topnav") {
     if (prevScrollpos > currentScrollPos) {
       navbar.style.top = "0";
-      } else {
+      } else{
       navbar.style.top = "-110px";
       }
     }
     else null;
   prevScrollpos = currentScrollPos;
 }
+
+
 
