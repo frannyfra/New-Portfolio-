332b4889baabcf6aec8987cfa40972befa9c1186
frannyfra <francescadelaurentis1987@gmail.com>
2021-02-07 19:06:29 +0000

    Minor changes to last 3 elements of the grid- change of design

--- myscript.js
+++ myscript.js
@@ -1,3 +1,5 @@
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
 const toggle = () => {
   let element = document.getElementById("myTopnav");
@@ -7,31 +9,57 @@ const toggle = () => {
     element.className = "topnav";
 }
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  if (window.matchMedia("(min-width : 1250px)").matches) {
+    scrollNavbar()
+  }
+  else { scrollFunction() }
+};
+
+// button scroll function 
+const scrollFunction = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+const topFunction = () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
 let prevScrollpos = window.pageYOffset;
-window.onscroll = () => {
-let currentScrollPos = window.pageYOffset;
-const navbar = document.getElementById("myTopnav");
+const scrollNavbar = () => {
+  let currentScrollPos = window.pageYOffset;
+  const navbar = document.getElementById("myTopnav");
   if (navbar.className === "topnav") {
     if (prevScrollpos > currentScrollPos) {
       navbar.style.top = "0";
-      } else{
+    } else {
       navbar.style.top = "-110px";
-      }
     }
-    else null;
+  }
+  else null;
   prevScrollpos = currentScrollPos;
 }
 
+
+
+// tesxt slider
 let slideIndex = 1;
-const showSlides =(n)=>  {
- let i;
+const showSlides = (n) => {
+  let i;
   let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex - 1].style.display = "block";
 }
 showSlides(slideIndex);
 
@@ -42,5 +70,7 @@ const plusSlides = (n) => {
 const currentSlide = (n) => {
   showSlides(slideIndex = n);
 }
+
+
 
 
