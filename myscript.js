828190d076ec92656c7eb9088a7dd983bc49c708
frannyfra <francescadelaurentis1987@gmail.com>
2021-02-07 15:40:04 +0000

    Replace video with text slider

--- myscript.js
+++ myscript.js
@@ -22,5 +22,25 @@ const navbar = document.getElementById("myTopnav");
   prevScrollpos = currentScrollPos;
 }
 
+let slideIndex = 1;
+const showSlides =(n)=>  {
+ let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+showSlides(slideIndex);
+
+const plusSlides = (n) => {
+  showSlides(slideIndex += n);
+}
+
+const currentSlide = (n) => {
+  showSlides(slideIndex = n);
+}
 
 
